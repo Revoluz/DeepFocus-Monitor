73b1925e4beb6221fb46b5b1558627e5e21623a0
Fanan <ahmadbantul3@gmail.com>
2026-07-06 11:55:04 +0700

    feat: remove LOOKING_AWAY (menoleh) status

--- config.py
+++ config.py
@@ -59,7 +59,6 @@ STATUS_COLORS = {
     'YAWNING': (0, 165, 255),
     'DROWSY': (0, 165, 255),
     'DISTRACTED': (0, 140, 255),
-    'LOOKING_AWAY': (0, 165, 255),
     'MICROSLEEP': (0, 0, 255),
     'PHONE_ALERT': (0, 0, 255),
     'FACE_LOST': (255, 255, 255),
